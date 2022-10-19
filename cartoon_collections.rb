f434ecc277410526d00e0ed367aff807fb68ed77
Vincent <makokhavomondi@gmail.com>
2022-10-19 23:28:07 +0300

    Completed Assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves (dwarves)# code an argument here
   # Your code here
+  puts dwarves.map.with_index {|dwarve, index| "#{index + 1}. #{dwarve}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (members)# code an argument here
   # Your code here
+  members.map {|member| "#{member.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls (calls_arr)# code an argument here
   # Your code here
+  calls_arr.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (str_arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  str_arr.find do |item|
+    found_cheese = cheese_types.find {|cheese| cheese == item}
+    !found_cheese.nil?
+  end
 end
+
+# binding.pry
